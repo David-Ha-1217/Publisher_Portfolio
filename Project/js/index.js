d59f267092d625a715f02f8a33ab78c7e4ec1efe
David-Ha-1217 <dhlvyou@gmail.com>
2020-06-11 18:13:25 +0900

    (2020-06-11) index.html 슬라이더 배너.

--- Project/js/index.js
+++ Project/js/index.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-    $(".nav li").hover(function() {
-        $(this).children("ul").stop().slideToggle(500)
+    var slider = $("#slide_banner").bxSlider ({
+        mode : "horizontal",
+        speed : 500,
+        pager : false,
+        moveSlides:1,
+        slideWidth:216,
+        minSlides:5,
+        maxSlides:5,
+        auto:true,
+        autoHover:true,
+        controls:false,
+        ariaLive:true
+    });
+
+    $("#prevBtn").on("click",function(){
+        slider.goToPrevSlide();
+        return false;
+    });
+
+    $("#nextBtn").on("click",function(){
+        slider.goToNextSlide();
+        return false;
     });
 });
